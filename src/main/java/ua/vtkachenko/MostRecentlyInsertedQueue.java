ac9c3c95559c90233baad64c9d4d0a3582ff58f6
McLoy <tkachenko.vlad@hotmail.com>
2016-12-06 21:01:38 +0200

    Create

---
+++ src/main/java/ua/vtkachenko/MostRecentlyInsertedQueue.java
@@ -0,0 +1,128 @@
+package ua.vtkachenko;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class MostRecentlyInsertedQueue<E> extends AbstractQueue<E> implements Queue<E> {
+
+    private Entry header;
+    private int capacity;
+    private int size = 0;
+
+    public MostRecentlyInsertedQueue(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        header = new Entry(null, header, header);
+        header.next = header.prev = header;
+        this.capacity = capacity;
+        this.size = 0;
+    }
+
+    @Override
+    public void clear() {
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            poll();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            if (str.equals("")) {
+                str = str + it.next();
+            } else {
+                str = str + ", " + it.next();
+            }
+        }
+        return "[ " + str + " ]";
+    }
+
+    private static class Entry<T> {
+        T element;
+        Entry<T> next;
+        Entry<T> prev;
+
+        Entry(T element, Entry<T> next, Entry<T> prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private class MostRecentlyInsertedQueueIterator<T> implements Iterator<T> {
+
+        private Entry<T> cursor = header;
+
+        @Override
+        public boolean hasNext() {
+
+            return cursor.next != header && cursor.next != cursor;
+        }
+
+        @Override
+        public T next() {
+            if (cursor.next != header && cursor.next != cursor) {
+                cursor = cursor.next;
+                return cursor.element;
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MostRecentlyInsertedQueueIterator();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        if (e == null) {
+            throw new NullPointerException();
+        }
+        if (size >= capacity) {
+            poll();
+        }
+        addEntry(e);
+        return size > 0;
+    }
+
+    private void addEntry(E e) {
+        Entry newEntry = new Entry(e, header, header.prev);
+        newEntry.prev.next = newEntry;
+        newEntry.next.prev = newEntry;
+        size++;
+    }
+
+    @Override
+    public E poll() {
+        Entry entry = header.next;
+        E value = (E) entry.element;
+        clearInnerLinks(entry);
+        return value;
+    }
+
+    private void clearInnerLinks(Entry<E> x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+        x.next = null;
+        x.prev = null;
+        x.element = null;
+        size--;
+    }
+
+    @Override
+    public E peek() {
+        return (E) header.next.element;
+    }
+}
---
+++ src/test/java/ua/vtkachenko/MostRecentlyInsertedQueueTest.java
@@ -0,0 +1,71 @@
+package ua.vtkachenko;
+
+import org.fest.assertions.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Queue;
+
+public class MostRecentlyInsertedQueueTest {
+
+    private Queue<Integer> queue;
+
+    @Before
+    public void setUp() throws Exception {
+        queue = new MostRecentlyInsertedQueue<>(3);
+    }
+
+    @Test
+    public void size() throws Exception {
+        Assertions.assertThat(queue.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void offer() throws Exception {
+        queue.offer(1);
+        Assertions.assertThat(queue.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void peek() throws Exception {
+        queue.offer(2);
+        queue.offer(3);
+        Assertions.assertThat(queue.peek()).isEqualTo(2);
+    }
+
+    @Test
+    public void poll() throws Exception {
+        queue.offer(4);
+        queue.offer(5);
+        Assertions.assertThat(queue.poll()).isEqualTo(4);
+    }
+
+    @Test
+    public void hugeOffer() throws Exception {
+        queue.offer(6);
+        queue.offer(7);
+        queue.offer(8);
+        queue.offer(9);
+        Assertions.assertThat(queue.peek()).isEqualTo(7);
+    }
+
+    @Test
+    public void clear() throws Exception {
+        queue.offer(10);
+        queue.offer(11);
+        queue.clear();
+        Assertions.assertThat(queue.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void writeString() throws Exception {
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+        queue.poll();
+        queue.poll();
+        Assertions.assertThat(queue.toString()).isEqualTo("[ 5 ]");
+    }
+}
